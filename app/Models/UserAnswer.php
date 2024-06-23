8b93e58b489e50f2722f8e77a3a10393e8f0c9e0
akhaled <akhaledlarus@gmail.com>
2024-06-24 02:06:28 +0300

    added a relationship between between users and the questions he answered

---
+++ app/Models/UserAnswer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserAnswer extends Model
+{
+    protected $table = "user_answer";
+    const UPDATED_AT = null;
+    const CREATED_AT = null;
+    protected $fillable = ["user_id", "question_id"];
+}
---
+++ database/migrations/2024_06_23_230206_create_user_answer_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('user_answer', function (Blueprint $table) {
+            $table->string("user_id");
+            $table->integer("question_id");
+
+            $table->foreign("user_id")->references("user_id")->on("user");
+            $table->foreign("question_id")->references("question_id")->on("question");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_answer');
+    }
+};
